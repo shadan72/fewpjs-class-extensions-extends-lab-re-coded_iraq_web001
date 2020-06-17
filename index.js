93097f88a3464c321e432bc2e2774740eb2dbf25
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-17 13:32:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+class Polygon{
+  constructor(arr){
+    this.array=arr;
+  }
+  get countSides(){
+   return this.array.reduce( (acc) => acc+1 , 0)
+  }
+  get perimeter(){
+    return this.array.reduce((acc,val) => {
+      return acc+val
+    },0)
+  }
+}
+class Triangle extends Polygon{
+  
+  get isValid(){
+    if(this.array.length === 3)
+    return true
+  
+     else {
+    return false;
+  }
+  }
+}
