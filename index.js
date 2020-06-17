06b81d0218cd05d1a463167933b24795e3c6d581
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-17 13:38:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ class Triangle extends Polygon{
 
 class Square extends Polygon{
   get isValid(){
-    if( this.array.length ===4 && this.array[0] === this.array[1] )
+    if( this.array.length ===4 && this.array[0] === this.array[1] && this.array[0] === this.array[2] && this.array[0] === this.array[3] && this.array[1] === this.array[2] && this.array[1] === this.array[3] && this.array[2] === this.array[3])
     return true
     else{
       return false
